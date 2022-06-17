8439c91e9d3c3e1368a98bcc393516ee10df5452
nyawiradev <njaginjoyce@gmail.com>
2022-06-17 09:45:27 +0300

    commit tree dad52ac453bff0b8c72ad2db10786fd19387f549

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  // const fetchedData = fetch('https://anapioficeandfire.com/api/books');
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then((response) => {
+    return response.json();
+  })
 }
+then((JsonData) => {
+  return renderBooks(JsonData);
+});
+// return fetchedData.json
 
-function renderBooks(books) {
+function renderBooks (books) {
   const main = document.querySelector('main');
-  books.forEach(book => {
+  books.forEach(book=> {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
